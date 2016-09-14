2b4eb9e9df795148c46256d86d829c471ab6b11f
Vito Kortbeek <vito.kortbeek@gmail.com>
2016-09-15 00:50:11 +0200

    Removes resolution restrictions from AssetLoader
    The resolution restrictions in form of fixed numbers has been removed by
    putting the needed ones into the Config class.

--- core/src/com/group66/game/settings/Config.java
+++ core/src/com/group66/game/settings/Config.java
@@ -6,23 +6,32 @@ package com.group66.game.settings;
 public class Config {
 	/* Resolution settings */
 	// TODO make a bit nicer
+	/** The Constant for the border size BORDER_SIZE_X (the sides). */
+	public static final int BORDER_SIZE_SIDES = 10;
+	
+	/** The Constant for the border size BORDER_SIZE_TOP (the top). */
+	public static final int BORDER_SIZE_TOP = 10;
+	
+	/** The Constant for the border size BORDER_SIZE_BOT (the bottom). */
+	public static final int BORDER_SIZE_BOT = 10;
+			
 	/** The Constant for the game WIDTH. */
-	public static final int WIDTH = 128 + 20;
+	public static final int WIDTH = 600 + BORDER_SIZE_SIDES*2;
 	
 	/** The Constant for the game HEIGHT. */
-	public static final int HEIGHT = 220 + 20;
+	public static final int HEIGHT = 880 + BORDER_SIZE_TOP + BORDER_SIZE_BOT;
 	
 	/** The Constant for the ball bounce border BOUNCE_X_MIN. */
-	public static final int BOUNCE_X_MIN = 10;
+	public static final int BOUNCE_X_MIN = BORDER_SIZE_SIDES;
 	
 	/** The Constant for the ball bounce border BOUNCE_X_MAX. */
-	public static final int BOUNCE_X_MAX = WIDTH - 10;
+	public static final int BOUNCE_X_MAX = WIDTH - BORDER_SIZE_SIDES;
 
 	/** The Constant for the ball bounce border BOUNCE_Y_MIN. */
-	public static final int BOUNCE_Y_MIN = 10;
+	public static final int BOUNCE_Y_MIN = BORDER_SIZE_BOT;
 	
 	/** The Constant for the ball bounce border BOUNCE_Y_MAX. */
-	public static final int BOUNCE_Y_MAX = HEIGHT - 10;
+	public static final int BOUNCE_Y_MAX = HEIGHT - BORDER_SIZE_TOP;
 	
 	/* Ball settings */
 	/** The Constant for the BALL_SPEED. */
@@ -30,6 +39,9 @@ public class Config {
 	
 	/** The Constant for the BALL_RAD (radius). */
 	public static final int BALL_RAD = 8;
+	
+	/** The Constant for the BALL_DIAM (diameter). */
+	public static final int BALL_DIAM = BALL_RAD*2;
 	
 	/* Cannon settings */
 	/** The Constant for theCANNON_AIM_DELTA (the step size of a cannon aim adjust). */
