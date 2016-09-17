b29ffd2a73cd72e9b88703644b12b87960ee78a8
Antony Löbker <aelobker@gmail.com>
2016-09-17 14:49:40 +0200

    Audiomanager is now checkstyle, findbugs and pmd compliant.
    Added two scripts for checking. The first checks, the second gives the output.

--- core/src/com/group66/game/helpers/AudioManager.java
+++ core/src/com/group66/game/helpers/AudioManager.java
@@ -9,6 +9,9 @@ public class AudioManager {
     private static Sound shoot, wallhit, ballpop;
     private static Music gameMusic;
     
+    /**
+     * Load all the audioclips from files
+     */
     public static void load() {
         mute = false;
         shoot = Gdx.audio.newSound(Gdx.files.internal("audio/shoot.wav"));
@@ -19,38 +22,68 @@ public class AudioManager {
         gameMusic.setLooping(true);
     }
     
+    /**
+     * Mute the music and sounds
+     */
     public static void mute() {
         mute = true;
         stopMusic();
     }
     
+    /**
+     * Unmute the music and sounds
+     */
     public static void unmute() {
         mute = false;
         startMusic();
     }
     
+    /**
+     * Start the background music
+     */
     public static void startMusic() {
-        if(!mute) {
+        if (!mute) {
             gameMusic.play();
         }
     }
     
+    /**
+     * Stop the background music
+     */
     public static void stopMusic() {
         gameMusic.stop();
     }
     
+    /**
+     * Play the sound for shooting
+     */
     public static void shoot() {
-        shoot.play();
+        if (!mute) {
+            shoot.play();
+        }
     }
     
+    /**
+     * Play the sound for when a ball hits the wall
+     */
     public static void wallhit() {
-        wallhit.play();
+        if (!mute) {
+            wallhit.play();
+        }
     }
     
+    /**
+     * Play the sound for when a ball pops
+     */
     public static void ballpop() {
-        ballpop.play();
+        if (!mute) {
+            ballpop.play();
+        }
     }
     
+    /**
+     * Dispose of all audioclips
+     */
     public static void dispose() {
         shoot.dispose();
         wallhit.dispose();
