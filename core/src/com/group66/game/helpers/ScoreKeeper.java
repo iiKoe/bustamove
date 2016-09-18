bf1be6c8e4ac7f891782c1af653da59a1b6f19b7
Omar Hommos <11maxed11@gmail.com>
2016-09-18 16:44:26 +0200

    Fixed raising a number ot the power

--- core/src/com/group66/game/helpers/ScoreKeeper.java
+++ core/src/com/group66/game/helpers/ScoreKeeper.java
@@ -33,7 +33,7 @@ public class ScoreKeeper {
 	 * @param hangingBalls the hanging balls
 	 */
 	public void setCurrentScore(int poppingBalls, int hangingBalls) {
-		this.currentScore = this.currentScore + poppingBalls * 10 + (2 ^ hangingBalls) * 10;
+		this.currentScore = this.currentScore + poppingBalls * 10 + ((int)java.lang.Math.pow(2.0,hangingBalls)) * 10;
 	}
 	
 	// GameScreen.scoreKeeper.setCurrentScore(poppingBalls, hangingBalls);
