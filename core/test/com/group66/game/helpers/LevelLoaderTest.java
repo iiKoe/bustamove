3769741f627fc73c52a91990e02d2355fa1ca4d5
Antony Löbker <aelobker@gmail.com>
2016-10-20 19:24:48 +0200

    Fix high score manager for two player screen

--- core/test/com/group66/game/helpers/LevelLoaderTest.java
+++ core/test/com/group66/game/helpers/LevelLoaderTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import com.group66.game.cannon.BallManager;
+import com.group66.game.settings.DynamicSettings;
 
 public class LevelLoaderTest {
-    /*
     @Test(expected=NullPointerException.class)
     public void nullTest() {
         LevelLoader.loadLevel(null, false);
@@ -15,7 +15,8 @@ public class LevelLoaderTest {
     
     @Test
     public void loadFileTest() {
-        BallManager ballManager = new BallManager();
+        DynamicSettings dynamicSettings = new DynamicSettings();
+        BallManager ballManager = new BallManager(dynamicSettings);
         LevelLoader.loadLevel(ballManager, false);
         
         assertTrue(ballManager.getBallCount() > 0);
@@ -23,10 +24,10 @@ public class LevelLoaderTest {
     
     @Test
     public void generateTest() {
-        BallManager ballManager = new BallManager();
+        DynamicSettings dynamicSettings = new DynamicSettings();
+        BallManager ballManager = new BallManager(dynamicSettings);
         LevelLoader.generateLevel(ballManager, false);
         
         assertTrue(ballManager.getBallCount() > 0);
     }
-    */
 }
