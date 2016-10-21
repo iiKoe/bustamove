80e7ff3d3b20a4bde52dfd2ec3021e597cdfde5f
Vito Kortbeek <vito.kortbeek@gmail.com>
2016-10-21 02:49:59 +0200

    Revert "NOT WORKING"
    This reverts commit 35ec0ae4355eb92402f8e70b1ec751acbba97363.

--- core/src/com/group66/game/cannon/BallGraphBreadthFirstConditionalIterator.java
+++ core/src/com/group66/game/cannon/BallGraphBreadthFirstConditionalIterator.java
@@ -12,120 +12,112 @@ public class BallGraphBreadthFirstConditionalIterator implements Iterator<Ball> 
 
     /** List of all objects the iterator needs to give. */
     private ArrayList<Ball> list = new ArrayList<Ball>();
-    
-    Queue<Ball> queue = new LinkedList<Ball>();
 
     /**
      * Instantiates the iterator
      * @param graph  which the iterator should iterate over
      * @param start where the iterator should start to iterate.
      */
-    public BallGraphBreadthFirstConditionalIterator(UndirectedGraph<Ball, DefaultEdge> graph, TopBall start) {
+    public BallGraphBreadthFirstConditionalIterator(UndirectedGraph<Ball, DefaultEdge> graph, Ball start) {
+
+        //queue of balls to be processed
+        Queue<Ball> queue = new LinkedList<Ball>();
+
         //Add the ball that is given as parameter
-        list.add(start);
-        queue.add(start);
-    }
-    
-    /**
-     * Instantiates the iterator
-     * @param graph  which the iterator should iterate over
-     * @param start where the iterator should start to iterate.
-     */
-    public BallGraphBreadthFirstConditionalIterator(UndirectedGraph<Ball, DefaultEdge> graph, ColoredBall start) {
-        Queue<Ball> ballsToCheckAdjacenBalls = new LinkedList<Ball>();
-        ballsToCheckAdjacenBalls.add(start);
-        while (!queue.isEmpty()) {
-            Ball qball = queue.remove();
-            for (DefaultEdge e : graph.edgesOf(qball)) {
-                //Check target of the edge
-                Ball eball = graph.getEdgeTarget(e);
-                if (eball instanceof BombBall && !list.contains(eball)) {
-                    queue.add(eball);
-                    ballsToCheckAdjacenBalls.add(eball);
-                    list.add(eball);
-                }
-                //check source of the edge
-                eball = graph.getEdgeSource(e);
-                if (eball instanceof BombBall && !list.contains(eball)) {
-                    queue.add(eball);
-                    ballsToCheckAdjacenBalls.add(eball);
-                    list.add(eball);
+        if (!(start instanceof TopBall)) {
+            list.add(start);
+            queue.add(start);
+        }
+        if (!(start instanceof BombBall)) {
+            //Process all the balls in the queue
+            while (!queue.isEmpty()) {
+                Ball qball = queue.remove();
+                //investigate all the edges of the ball
+                for (DefaultEdge e : graph.edgesOf(qball)) {
+                    //Check target of the edge
+                    Ball eball = graph.getEdgeTarget(e);
+                    if (eball.isEqual(start) && !list.contains(eball)) {
+                        queue.add(eball);
+                        list.add(eball);
+                    }
+                    //check source of the edge
+                    eball = graph.getEdgeSource(e);
+                    if (eball.isEqual(start) && !list.contains(eball)) {
+                        queue.add(eball);
+                        list.add(eball);
+                    }
                 }
             }
-        }
+        } else {
+            Queue<Ball> ballsToCheckAdjacenBalls = new LinkedList<Ball>();
+            ballsToCheckAdjacenBalls.add(start);
+            while (!queue.isEmpty()) {
+                Ball qball = queue.remove();
+                for (DefaultEdge e : graph.edgesOf(qball)) {
+                    //Check target of the edge
+                    Ball eball = graph.getEdgeTarget(e);
+                    if (eball instanceof BombBall && !list.contains(eball)) {
+                        queue.add(eball);
+                        ballsToCheckAdjacenBalls.add(eball);
+                        list.add(eball);
+                    }
+                    //check source of the edge
+                    eball = graph.getEdgeSource(e);
+                    if (eball instanceof BombBall && !list.contains(eball)) {
+                        queue.add(eball);
+                        ballsToCheckAdjacenBalls.add(eball);
+                        list.add(eball);
+                    }
+                }
+            }
 
-        while (!ballsToCheckAdjacenBalls.isEmpty() && list.size() > 1) {
-            Ball qball = ballsToCheckAdjacenBalls.remove();
-            //investigate all the edges of the ball
-            for (DefaultEdge e : graph.edgesOf(qball)) {
-                //Check target of the edge
-                Ball eball = graph.getEdgeTarget(e);
-                Iterator<Ball> iterator = new BallGraphAdjacentIterator(graph, eball);
+            while (!ballsToCheckAdjacenBalls.isEmpty() && list.size() > 1) {
+                Ball qball = ballsToCheckAdjacenBalls.remove();
+                //investigate all the edges of the ball
+                for (DefaultEdge e : graph.edgesOf(qball)) {
+                    //Check target of the edge
+                    Ball eball = graph.getEdgeTarget(e);
+                    Iterator<Ball> iterator = new BallGraphAdjacentIterator(graph, eball);
 
-                while (iterator.hasNext()) {
-                    Ball adjacentBall = iterator.next();
-                    if (!(adjacentBall instanceof BombBall)) {
-                        BallGraphBreadthFirstConditionalIterator checkIterator = 
-                                new BallGraphBreadthFirstConditionalIterator(graph, adjacentBall);
-                        if (checkIterator.size() >= 2) {
-                            while (checkIterator.hasNext()) {
-                                Ball next = checkIterator.next();
-                                if (!list.contains(next)) {
-                                    list.add(next);
-                                }            
+                    while (iterator.hasNext()) {
+                        Ball adjacentBall = iterator.next();
+                        if (!(adjacentBall instanceof BombBall)) {
+                            BallGraphBreadthFirstConditionalIterator checkIterator = 
+                                    new BallGraphBreadthFirstConditionalIterator(graph, adjacentBall);
+                            if (checkIterator.size() >= 2) {
+                                while (checkIterator.hasNext()) {
+                                    Ball next = checkIterator.next();
+                                    if (!list.contains(next)) {
+                                        list.add(next);
+                                    }            
+                                }
+                            }
+                        }
+                    }
+
+                    //check source of the edge
+                    eball = graph.getEdgeSource(e);
+                    iterator = new BallGraphAdjacentIterator(graph, eball);
+
+                    while (iterator.hasNext()) {
+                        Ball adjacentBall = iterator.next();
+                        if (!(adjacentBall instanceof BombBall)) {
+                            BallGraphBreadthFirstConditionalIterator checkIterator = 
+                                    new BallGraphBreadthFirstConditionalIterator(graph, adjacentBall);
+                            if (checkIterator.size() >= 2) {
+                                while (checkIterator.hasNext()) {
+                                    Ball next = checkIterator.next();
+                                    if (!list.contains(next)) {
+                                        list.add(next);
+                                    }            
+                                }
                             }
                         }
                     }
                 }
-
-                //check source of the edge
-                eball = graph.getEdgeSource(e);
-                iterator = new BallGraphAdjacentIterator(graph, eball);
-
-                while (iterator.hasNext()) {
-                    Ball adjacentBall = iterator.next();
-                    if (!(adjacentBall instanceof BombBall)) {
-                        BallGraphBreadthFirstConditionalIterator checkIterator = 
-                                new BallGraphBreadthFirstConditionalIterator(graph, adjacentBall);
-                        if (checkIterator.size() >= 2) {
-                            while (checkIterator.hasNext()) {
-                                Ball next = checkIterator.next();
-                                if (!list.contains(next)) {
-                                    list.add(next);
-                                }            
-                            }
-                        }
-                    }
-                }
             }
         }
-    }
-    
-    /**
-     * Instantiates the iterator
-     * @param graph  which the iterator should iterate over
-     * @param start where the iterator should start to iterate.
-     */
-    public BallGraphBreadthFirstConditionalIterator(UndirectedGraph<Ball, DefaultEdge> graph, BombBall start) {
-        //Process all the balls in the queue
-        while (!queue.isEmpty()) {
-            Ball qball = queue.remove();
-            //investigate all the edges of the ball
-            for (DefaultEdge e : graph.edgesOf(qball)) {
-                //Check target of the edge
-                Ball eball = graph.getEdgeTarget(e);
-                if (eball.isEqual(start) && !list.contains(eball)) {
-                    queue.add(eball);
-                    list.add(eball);
-                }
-                //check source of the edge
-                eball = graph.getEdgeSource(e);
-                if (eball.isEqual(start) && !list.contains(eball)) {
-                    queue.add(eball);
-                    list.add(eball);
-                }
-            }
-        }
+
     }
 
     /**
