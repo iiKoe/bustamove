c6f355360910c00e499d6b8e0362058cb2008e5d
jeroenoverman <jeroenoverman073@gmail.com>
2016-10-25 21:13:46 +0200

    dynamicSettings is now created by BustaMove.java

--- core/src/com/group66/game/BustaMove.java
+++ core/src/com/group66/game/BustaMove.java
@@ -10,6 +10,7 @@ import com.group66.game.logging.Logger;
 import com.group66.game.logging.MessageType;
 import com.group66.game.screens.StartScreen;
 import com.group66.game.settings.Config;
+import com.group66.game.settings.DynamicSettings;
 
 /**
  * The BustaMove main game class.
@@ -22,7 +23,11 @@ public class BustaMove extends Game {
     /** The logger. */
     private Logger logger;
     
+    /** The high score manager. */
     private HighScoreManager highScoreManager;
+    
+    /** The dynamic settings object. */
+    private DynamicSettings dynamicSettings;
     
     /** Create the only object of this class */
     private static BustaMove game = new BustaMove();
@@ -87,6 +92,14 @@ public class BustaMove extends Game {
         return highScoreManager;
     }
 
+    /**
+     * Gets the dynamic settings
+     * @return the dynamic settings
+     */
+    public DynamicSettings getDynamicSettings() {
+        return dynamicSettings;
+    }
+
     /* (non-Javadoc)
      * @see com.badlogic.gdx.ApplicationListener#create()
      */
@@ -94,6 +107,7 @@ public class BustaMove extends Game {
     public void create() {
         batch = new SpriteBatch();
         highScoreManager = new HighScoreManager();
+        dynamicSettings = new DynamicSettings();
         
         Logger fileLogger = new FileLogger(MessageType.Debug);
         Logger consoleLogger = new ConsoleLogger(MessageType.Info);
