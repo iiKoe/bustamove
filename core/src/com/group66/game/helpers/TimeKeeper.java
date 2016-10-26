800f1ad9bb4ffa0c6b6d3580dc65e81b431a6fae
jeroenoverman <jeroenoverman073@gmail.com>
2016-10-27 01:22:39 +0200

    fixed unnescesarry log.

--- core/src/com/group66/game/helpers/TimeKeeper.java
+++ core/src/com/group66/game/helpers/TimeKeeper.java
@@ -44,7 +44,6 @@ public class TimeKeeper {
      */
     public void shotTimeReset() {
         this.lastShotTime = this.universalTime;
-        System.out.println("Last shot time Reset!");
     }
     
     /**
