22f9daec25f9269f1f881f1891cb97612e306391
jeroenoverman <jeroenoverman073@gmail.com>
2016-11-04 02:00:01 +0100

    removed unused import

--- core/test/com/group66/game/settings/DynamicSettingsTest.java
+++ core/test/com/group66/game/settings/DynamicSettingsTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import com.group66.game.BustaMove;
-
 public class DynamicSettingsTest {
     @Test
     public void creationTest() {
