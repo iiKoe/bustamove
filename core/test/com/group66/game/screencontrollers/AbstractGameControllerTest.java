c967fb4f5639a2f1c8c0c7c9cac205a4706f24cd
jeroenoverman <jeroenoverman073@gmail.com>
2016-11-04 14:03:50 +0100

    try to fix travis

--- core/test/com/group66/game/screencontrollers/AbstractGameControllerTest.java
+++ core/test/com/group66/game/screencontrollers/AbstractGameControllerTest.java
@@ -2,14 +2,19 @@ package com.group66.game.screencontrollers;
 
 import org.junit.Test;
 
+import com.group66.game.BustaMove;
 import com.group66.game.cannon.GameManager;
-import com.group66.game.screencontrollers.AbstractGameController.GameState;
 
 public abstract class AbstractGameControllerTest {
     protected abstract AbstractGameController getGameController();
     
     @Test
     public void pauseTest() {
+        try {
+            BustaMove.getGameInstance().create();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         AbstractGameController controller = getGameController();
         controller.togglePause();
         controller.update(0);
@@ -17,12 +22,22 @@ public abstract class AbstractGameControllerTest {
     
     @Test
     public void updateTest() {
+        try {
+            BustaMove.getGameInstance().create();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         AbstractGameController controller = getGameController();
         controller.update(0);
     }
     
     @Test
     public void winTest() {
+        try {
+            BustaMove.getGameInstance().create();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         AbstractGameController controller = getGameController();
         try {
             GameManager gameManager = controller.getGameManager1();
@@ -35,6 +50,11 @@ public abstract class AbstractGameControllerTest {
     
     @Test
     public void loseTest() {
+        try {
+            BustaMove.getGameInstance().create();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         AbstractGameController controller = getGameController();
         try {
             GameManager gameManager = controller.getGameManager1();
--- core/test/com/group66/game/screencontrollers/OnePlayerGameControllerTest.java
+++ core/test/com/group66/game/screencontrollers/OnePlayerGameControllerTest.java
@@ -2,14 +2,26 @@ package com.group66.game.screencontrollers;
 
 import org.junit.Test;
 
+import com.group66.game.BustaMove;
+
 public class OnePlayerGameControllerTest extends AbstractGameControllerTest {
     @Override
     protected AbstractGameController getGameController() {
+        try {
+            BustaMove.getGameInstance().create();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return new OnePlayerGameController(false, 3);
     }
     
     @Test
     public void randomTest() {
+        try {
+            BustaMove.getGameInstance().create();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         new OnePlayerGameController(true, 1);
     }
 }
